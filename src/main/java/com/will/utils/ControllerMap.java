d2e2a94bee6dbc3cbcbd111aab631c8246092a0c
Wilson Filho <will.filho27@gmail.com>
2016-06-17 17:59:48 -0300

    Classes Utils para os Controllers
    Criando as classes que vão ajudar no mapeando dos Controllers.

---
+++ src/main/java/com/will/utils/ControllerMap.java
@@ -0,0 +1,8 @@
+package com.will.utils;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(consumes = "application/json", produces = "application/json")
+public interface ControllerMap {
+
+}
---
+++ src/main/java/com/will/utils/ControllerPath.java
@@ -0,0 +1,21 @@
+package com.will.utils;
+
+public class ControllerPath {
+	
+	/** Criando as constantes que vão armazenar as URI do sistema. 
+	 * Dessa forma não precisa alterar nos controllers quando precisar mudar URI.
+	*/
+	
+	public static final String ALL =" /** ";
+	
+	public static final String ROOT_PATH =" /api ";
+	
+	public static final String PUBLIC_ROOT_PATH = ROOT_PATH +" /public ";
+	
+	public static final String PRIVATE_ROOT_PATH = ROOT_PATH +" /private ";
+	
+	public static final String CLIENTE_PATH = PRIVATE_ROOT_PATH +" /cliente ";
+	
+	public static final String PLANO_ACESSO_PATH = PRIVATE_ROOT_PATH +" /plano ";
+
+}
